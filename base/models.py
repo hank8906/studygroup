9d4ca06a38bb33cc3e117dbe20217cfff112be50
hank <huanghank2000@gmail.com>
2023-03-22 18:40:53 +0800

    Database & Admin Panel

--- base/models.py
+++ base/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+# 定義Topic的Ｍodel
+class Topic(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+      return self.name
+
+# 定義Room的Ｍodel
+class Room(models.Model):
+    host = models.ForeignKey(User, on_delete=models.SET_NULL,null = True)
+    topic = models.ForeignKey(Topic, on_delete=models.SET_NULL,null = True)
+    name = models.CharField(max_length=200)
+    # null=True : sets NULL (versus NOT NULL) on the column in DB
+    # blank=True : field types such as DateTimeField,ForeignKey will be stored as NULL in DB.
+    description = models.TextField(null=True, blank=True)
+    # participants =
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return self.name
+
+# 定義Message的Ｍodel
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return self.body[0:50]
