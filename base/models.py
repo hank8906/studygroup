10eed341d61b63af1c6a6084e6da0206aae94d88
hank <huanghank2000@gmail.com>
2023-03-23 18:08:33 +0800

    CRUD

--- base/models.py
+++ base/models.py
@@ -21,6 +21,8 @@ class Room(models.Model):
     # participants =
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    class Meta:
+      ordering = ['-updated','-created']
 
     def __str__(self):
       return self.name
@@ -29,7 +31,7 @@ class Room(models.Model):
 class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
-    body = models.TextField
+    body = models.TextField(default='')
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Room
+from .forms import RoomForm
 
 # rooms = [
 #     {'id':1, 'name':'Lets learn python'},
@@ -7,12 +8,47 @@ from .models import Room
 #     {'id':3, 'name':'Lets learn python'},
 # ]
 
+# 設定主頁面
 def home(request):
     rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request,'base/home.html',context)
 
+# 設定每個房間頁面
 def room(request,pk):
     room =Room.objects.get(id=pk)
     context = {'room':room}
     return render(request,'base/room.html',context)
+
+# 設定新增房間頁面
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form' : form}
+    return render(request,'base/room_form.html',context)
+
+# 設定更新房間頁面
+def updateRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)
+    if request.method == 'POST':
+        form = RoomForm(request, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form' :form}
+    return render(request,'base/room_form.html',context)
+
+# 設定刪除房間頁面
+def deleteRoom(request,pk):
+    room = Room.objects.get(id=pk)
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request,'base/delete.html',{'obj':room})
